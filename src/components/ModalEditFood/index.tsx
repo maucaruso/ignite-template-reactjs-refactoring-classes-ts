f2a89913e6e0731b4bd6b1677d64067207b80184
maurici3 <carusojr@outlook.com>
2022-04-02 21:16:14 -0300

    chore: refact Food, Input, ModalAddFood components

--- src/components/ModalEditFood/index.tsx
+++ src/components/ModalEditFood/index.tsx
@@ -38,12 +38,12 @@ function ModalEditFood(props: ModalEditFoodProps) {
     <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
       <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
         <h1>Editar Prato</h1>
-        <Input name="image" placeholder="Cole o link aqui" icon="" />
+        <Input name="image" placeholder="Cole o link aqui" />
 
-        <Input name="name" placeholder="Ex: Moda Italiana" icon="" />
-        <Input name="price" placeholder="Ex: 19.90" icon="" />
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
 
-        <Input name="description" placeholder="Descrição" icon="" />
+        <Input name="description" placeholder="Descrição" />
 
         <button type="submit" data-testid="edit-food-button">
           <div className="text">Editar Prato</div>
